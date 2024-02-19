ec43333f5e5e5400ad611edacd93205eb9b7a57e
Lucas Skog <lucasskog@gmail.com>
2024-02-19 12:19:35 +0100

    Done with cafe-retro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar";
+import Section from "./components/Section";
+
 export function App() {
   return (
     <>
-      <h1>This is the App Component</h1>
+     <Navbar/>
+     <Section/>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <header>
+      <a href="#hot">Hot</a>
+      <a href="#juicy">Juicy</a>
+      <a href="#cosy">Cosy</a>
+    </header>
+  );
+}
